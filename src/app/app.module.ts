4fdd2f945e4834c82696e8e4f43f3cce56dfd92e
venkatesh <venkatesh.sv645@gmail.com>
2021-02-07 19:55:10 +0530

    header and footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'register', component: RegisterComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
