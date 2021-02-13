2ff9aa37a6b4ba4079e751efdf5e960603730765
venkatesh <venkatesh.sv645@gmail.com>
2021-02-13 10:59:47 +0530

    Working login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { HeaderComponent } from './common/header/header.component';
-import { FooterComponent } from './common/footer/footer.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MainComponent } from './common/main/main.component';
-import { ErrorFeedbackComponent } from './common/error-feedback/error-feedback.component';
+import { MainComponent } from './components/common/main/main.component';
+import { ErrorFeedbackComponent } from './components/common/error-feedback/error-feedback.component';
 
 const routes: Routes = [
   {
